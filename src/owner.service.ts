022c633160306d344f14e1c30b02a2bcd29a2221
Guilherme Vianna <mataveli91@gmail.com>
2023-12-13 19:01:11 -0300

    handling unique constraint error added

--- src/owner.service.ts
+++ src/owner.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpCode, Injectable } from "@nestjs/common";
 import { createOwnerDTO } from "./dto/createOwnerDTO";
 import { PrismaClient } from "@prisma/client";
 
@@ -7,16 +7,21 @@ export class OwnerService {
   constructor(private readonly prismaClient: PrismaClient) {}
 
   async register(dto: createOwnerDTO) {
-    const result = await this.prismaClient.owner.create({
-      data: {
-        name: dto.name,
-        email: dto.email,
-        birth_date: `${dto.birthdate}T00:00:00.000Z`,
-      },
-    });
-
-    await this.prismaClient.$disconnect();
-
-    return result;
+    try {
+      const result = await this.prismaClient.owner.create({
+        data: {
+          name: dto.name,
+          email: dto.email,
+          birth_date: `${dto.birthdate}T00:00:00.000Z`,
+        },
+      });
+      await this.prismaClient.$disconnect();
+      return result;
+    } catch (exception) {
+      if (exception.code === "P2002") {
+        await this.prismaClient.$disconnect();
+        return false;
+      }
+    }
   }
 }
