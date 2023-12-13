c78f7d437371b6a915ef966b6d8f6c8b864bb4e7
Guilherme Vianna <mataveli91@gmail.com>
2023-12-12 22:37:48 -0300

    owner register method added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { OwnerModule } from './owner.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(OwnerModule);
   await app.listen(3000);
 }
 bootstrap();
