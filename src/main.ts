e69895c1f11277b063494978497e8aed10c75834
Guilherme Vianna <mataveli91@gmail.com>
2023-12-20 11:56:00 -0300

    cors enable

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(OwnerModule);
+
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
   await app.listen(3001);
 }
 bootstrap();
