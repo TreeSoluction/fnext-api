012653d9b3ce3374ebef733092a3b3806c7b47ba
Guilherme Vianna <mataveli91@gmail.com>
2023-12-20 11:35:34 -0300

    default listen port changed to 3001

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { OwnerModule } from './owner.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestFactory } from "@nestjs/core";
+import { OwnerModule } from "./owner.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(OwnerModule);
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
