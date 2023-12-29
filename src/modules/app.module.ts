be3cc989d23c8809c44f58d9dcdffc93edb16165
Guilherme Mataveli <mataveli91@gmail.com>
2023-12-28 21:23:54 -0300

    account confirmation added

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { InvestorModule } from "./investor/investor.module";
 import { StartupModule } from "./startup/startup.module";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [InvestorModule, StartupModule],
+  imports: [InvestorModule, StartupModule, UserModule],
 })
 export class AppModule {}
