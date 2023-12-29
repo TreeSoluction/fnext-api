caab0816a92f2950d618b6eb0ad506161f09ccb6
Guilherme Mataveli <mataveli91@gmail.com>
2023-12-28 21:39:35 -0300

    code send  via email added

--- src/modules/startup/startup.module.ts
+++ src/modules/startup/startup.module.ts
@@ -3,9 +3,10 @@ import { StartupController } from "./startup.controller";
 import { PrismaClient } from "@prisma/client";
 import { StartupService } from "./startup.service";
 import { UserService } from "src/modules/user/user.service";
+import { EmailService } from "src/services/email/email.service";
 
 @Module({
   controllers: [StartupController],
-  providers: [StartupService, PrismaClient, UserService],
+  providers: [StartupService, PrismaClient, UserService, EmailService],
 })
 export class StartupModule {}
